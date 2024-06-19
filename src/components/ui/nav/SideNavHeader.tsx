dc4ed49ac5ad4741e0dfa0d3d3f5899f11f29b70
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-19 18:42:43 +0300

    refactor: Update SideNavHeader component button design

--- src/components/ui/nav/SideNavHeader.tsx
+++ src/components/ui/nav/SideNavHeader.tsx
@@ -1,24 +1,21 @@
-import { Avatar, Chip } from '@nextui-org/react';
-import React from 'react'
+import { Avatar } from "@nextui-org/react";
+import React from "react";
 
 const SideNavHeader = () => {
   return (
     <header className="flex items-center justify-center flex-col gap-3">
       <Avatar src="https://avatars.githubusercontent.com/u/4723117?v=4" />
       <div className="flex flex-col text-white items-center gap-1.5">
-        <p className=" text-white font-bold tracking-wide">Ahmed Lukman</p>
-        <Chip
-          variant="shadow"
-          classNames={{
-            base: "bg-gradient-to-br from-indigo-500 to-pink-600 border-small border-white/50 shadow-pink-500/20",
-            content: "drop-shadow shadow-black text-white text-xs",
-          }}
-        >
-          Developer
-        </Chip>
+        <p className=" text-white tracking-wide">Ahmed Lukman</p>
+        <span className="relative inline-flex  overflow-hidden rounded-xl p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full items-center justify-center rounded-xl bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            Developer
+          </span>
+        </span>
       </div>
     </header>
   );
-}
+};
 
-export default SideNavHeader
+export default SideNavHeader;
