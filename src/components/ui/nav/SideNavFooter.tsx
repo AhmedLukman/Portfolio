c2d50a9cff1658bb544e46caf77af922e08734b5
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-19 18:41:07 +0300

    refactor: Update SideNavFooter component with NextUI Button

--- src/components/ui/nav/SideNavFooter.tsx
+++ src/components/ui/nav/SideNavFooter.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import { Button } from "@nextui-org/react";
+import React from "react";
 
 const SideNavFooter = () => {
   return (
     <footer className="gap-4 flex items-center justify-center flex-col">
-      <button className="relative inline-flex h-12 overflow-hidden rounded-xl p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-xl bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-          Download CV
-        </span>
-      </button>
+      <Button
+        variant="shadow"
+        className="text-white bg-gradient-to-br from-indigo-500 to-pink-600 border-small border-white/50 shadow-pink-500/30"
+      >
+        Download CV
+      </Button>
     </footer>
   );
-}
+};
 
-export default SideNavFooter
+export default SideNavFooter;
