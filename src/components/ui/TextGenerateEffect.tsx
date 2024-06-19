8dc609ee93d7a7af4947de96e848df84139b6f54
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-19 15:48:16 +0300

    feat: Add TextGenerateEffect component for animating text

---
+++ src/components/ui/TextGenerateEffect.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { useEffect } from "react";
+import { motion, stagger, useAnimate } from "framer-motion";
+import { cn } from "@nextui-org/react";
+
+export const TextGenerateEffect = ({
+  words,
+  className,
+}: {
+  words: string;
+  className?: string;
+}) => {
+  const [scope, animate] = useAnimate();
+  let wordsArray = words.split(" ");
+  useEffect(() => {
+    animate(
+      "span",
+      {
+        opacity: 1,
+      },
+      {
+        duration: 2,
+        delay: stagger(0.2),
+      }
+    );
+  }, [animate]);
+
+  const renderWords = () => {
+    return (
+      <motion.div ref={scope}>
+        {wordsArray.map((word, idx) => {
+          return (
+            <motion.span
+              key={word + idx}
+              className="text-slate-400 opacity-0"
+            >
+              {word}{" "}
+            </motion.span>
+          );
+        })}
+      </motion.div>
+    );
+  };
+
+  return (
+    <div className={cn(className)}>
+      <div className="mt-4">
+        <div className="text-center text-sm italic">
+          {renderWords()}
+        </div>
+      </div>
+    </div>
+  );
+};
