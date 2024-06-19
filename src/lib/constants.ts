1f9c1c0402c105d56cd787f4c0dcbd9754152cab
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-19 15:57:11 +0300

    chore: Update constants.ts with web qualities and tech stack

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -41,3 +41,78 @@ export const SOCIAL_LINKS = [
     path: "https://www.linkedin.com/in/ahmedlukman",
   },
 ];
+
+export const WEB_QUALITIES = [
+  "Performant",
+  "Responsive",
+  "Aesthetic",
+  "Functional",
+  "Accessible",
+];
+
+export const TECH_STACK = [
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+    alt: "HTML5",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+    alt: "CSS3",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-plain.svg",
+    alt: "JavaScript",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+    alt: "React Js",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
+    alt: "Next Js",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
+    alt: "TypeScript",
+  },
+  {
+    src: "/assets/images/zod.svg",
+    alt: "Zod",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+    alt: "Tailwind CSS",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/prisma/prisma-original.svg",
+    alt: "Prisma",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg",
+    alt: "Figma",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg",
+    alt: "PostgreSQL",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg",
+    alt: "MongoDB",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg",
+    alt: "GitHub",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg",
+    alt: "VSCode",
+  },
+  {
+    src: "https://img.icons8.com/ios/100/ffffff/chatgpt.png",
+    alt: "Chatgpt",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vercel/vercel-original.svg",
+    alt: "Vercel",
+  },
+];
