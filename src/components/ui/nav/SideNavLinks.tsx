8a88ab04e7fa1104b47c06382003b1cb74e7847b
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-20 07:16:21 +0300

    fix: Add key prop to li when mapping

--- src/components/ui/nav/SideNavLinks.tsx
+++ src/components/ui/nav/SideNavLinks.tsx
@@ -9,8 +9,8 @@ const SideNavLinks = () => {
         <h2 className="text-white">Pages</h2>
         <ul className="flex flex-col gap-3">
           {PAGE_LINKS.map((link) => (
-            <li>
-              <SideNavLinkButton key={link.path} link={link} />
+            <li key={link.path}>
+              <SideNavLinkButton link={link} />
             </li>
           ))}
         </ul>
@@ -20,8 +20,8 @@ const SideNavLinks = () => {
         <h2 className="text-white">Socials</h2>
         <ul className="flex flex-col gap-3">
           {SOCIAL_LINKS.map((link) => (
-            <li>
-              <SideNavLinkButton key={link.path} link={link} isSocial />
+            <li key={link.path}>
+              <SideNavLinkButton link={link} isSocial />
             </li>
           ))}
         </ul>
