79794ba03f8281cce3017a29d40c8aa511823fa2
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-21 10:23:32 +0300

    refactor: Add li tag with z index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,8 @@ export default function Home() {
     <div className="mx-auto max-w-2xl flex h-screen py-20 flex-col">
       <section>
         <h1 className="text-3xl text-slate-200 text-center font-bold tracking-wide mb-1">
-          Ahmed Lukman <span className="purple-gradient-text">| Full Stack Developer</span>
+          Ahmed Lukman{" "}
+          <span className="purple-gradient-text">| Full Stack Developer</span>
         </h1>
         <GradientBorder />
         <p className=" text-slate-300 mt-8">
@@ -24,13 +25,21 @@ export default function Home() {
 
       <section className="mt-10 space-y-5">
         <h2 className=" text-xl text-slate-200 font-bold">Tech Stack</h2>
-        <div className="flex items-center gap-5 flex-wrap">
+        <ul className="flex items-center gap-5 flex-wrap">
           {TECH_STACK.map((tech) => (
-            <Tooltip color="secondary" key={tech.src} content={tech.alt}>
-              <Image priority alt={tech.alt} width={32} height={32} src={tech.src} />
-            </Tooltip>
+            <li key={tech.src} className="z-10">
+              <Tooltip color="secondary" className="z-10" content={tech.alt}>
+                <Image
+                  priority
+                  alt={tech.alt}
+                  width={32}
+                  height={32}
+                  src={tech.src}
+                />
+              </Tooltip>
+            </li>
           ))}
-        </div>
+        </ul>
       </section>
 
       <section className="mt-auto">
