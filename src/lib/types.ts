1d9feb05948514f1b287058b9a8d4211b4f41a03
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-22 19:22:30 +0300

    chore: Add Project type definition

---
+++ src/lib/types.ts
@@ -0,0 +1,13 @@
+type Project = {
+  title: string;
+  description: string;
+  image: string;
+  route: string;
+  site: string;
+  source: string;
+  slug: string;
+  techStack: {
+    alt: string;
+    src: string;
+  }[];
+};
