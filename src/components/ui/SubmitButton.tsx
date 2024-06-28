205798595445f8ec4aaf2f8b4bb8dded2ffed2c4
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-28 06:44:44 +0300

    feat: Add SubmitButton component for handling form submission

---
+++ src/components/ui/SubmitButton.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@nextui-org/react";
+import React from "react";
+import { useFormStatus } from "react-dom";
+
+const SubmitButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      type="submit"
+      isLoading={pending}
+      size="lg"
+      className="bg-slate-400 font-bold animate-slideInFromBottom opacity-0 delay-1000 shadow-[0_8px_16px_rgb(0_0_0/0.3)]"
+    >
+      Send
+    </Button>
+  );
+};
+
+export default SubmitButton;
