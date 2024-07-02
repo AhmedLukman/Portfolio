0fa0fbeb6ccb6e23b82f5b21c9ecf019ea6e84a5
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-07-02 18:39:04 +0300

    refactor: Update SubmitButton component to use FontAwesomeIcon for end content

--- src/components/ui/SubmitButton.tsx
+++ src/components/ui/SubmitButton.tsx
@@ -1,4 +1,6 @@
-import { Button } from "@nextui-org/react";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button, cn } from "@nextui-org/react";
 import React from "react";
 import { useFormStatus } from "react-dom";
 
@@ -9,7 +11,10 @@ const SubmitButton = () => {
       type="submit"
       isLoading={pending}
       size="lg"
-      className="bg-slate-400 font-bold animate-slideInFromBottom opacity-0 delay-1000 shadow-[0_8px_16px_rgb(0_0_0/0.3)] hover:!opacity-80"
+      className={cn("bg-slate-400 font-bold animate-slideInFromBottom opacity-0 delay-1000 shadow-[0_8px_16px_rgb(0_0_0/0.3)] hover:!opacity-80", {
+        "!opacity-80": pending,
+      })}
+      endContent={<FontAwesomeIcon icon={faEnvelope} />}
     >
       Send
     </Button>
