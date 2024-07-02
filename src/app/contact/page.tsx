09695e2a48e4ca06a9524426d8ee4e67640facb9
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-07-02 18:54:01 +0300

    feat: Add shaking hands illustration

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,7 @@
 import ContactForm from "@/components/ui/ContactForm";
 import GradientBorder from "@/components/ui/GradientBorder";
 import PageWrapper from "@/components/ui/PageWrapper";
+import Image from "next/image";
 import React from "react";
 
 const ContactPage = () => {
@@ -11,6 +12,14 @@ const ContactPage = () => {
       </h1>
       <GradientBorder className="max-w-xl mx-auto" />
       <ContactForm />
+      <Image
+        src="/assets/images/shakehands.png"
+        className="object-contain absolute right-0 bottom-0"
+        priority
+        width={300}
+        height={480}
+        alt="Shaking hands with contract illustration"
+      />
     </PageWrapper>
   );
 };
