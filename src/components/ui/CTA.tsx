aedb1917e6e2467c2da427030c9bef2e14ad8175
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-07-04 13:01:47 +0300

    refactor: Update CTA tag for semantic

--- src/components/ui/CTA.tsx
+++ src/components/ui/CTA.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { BackgroundGradient } from "./BackgroundGradient";
-import Image from "next/image";
 import { Button, Link } from "@nextui-org/react";
 
 const CTA = () => {
   return (
     <section className="mt-24">
       <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-black/70 flex flex-col items-center gap-3">
-        <p className="text-xl text-slate-200 font-bold text-center">
+        <h2 className="text-xl text-slate-200 font-bold text-center">
           Let&apos;s Work Together
-        </p>
+        </h2>
         <p className="text-lg text-slate-300 text-center mb-2 max-w-prose">
           Excited to start a new project? Collaborate on an existing one? Or
           just have a chat? Get in touch today!
