a9564d41e8e09003a6627ff4f6347e5b0d8fb5b7
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-07-04 12:52:27 +0300

    refactor: Update AnimatedWords component in Home page

--- src/components/page/home-page/AnimatedWords.tsx
+++ src/components/page/home-page/AnimatedWords.tsx
@@ -5,8 +5,10 @@ import React from 'react'
 const AnimatedWords = () => {
   return (
     <section className="mt-24">
-      <p className=" text-slate-400 text-2xl">Let&apos;s build </p>
-      <FlipWords className="text-white text-2xl" words={WEB_QUALITIES} />
+      <p className="text-slate-400 text-2xl">
+        Let&apos;s build <br />
+        <FlipWords words={WEB_QUALITIES} />
+      </p>
     </section>
   );
 }
