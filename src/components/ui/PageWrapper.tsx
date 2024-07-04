ac22d10bad73f63616d83348a98f02e88726c102
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-07-05 01:04:59 +0300

    style: Update PageWrapper component with smaller padding

--- src/components/ui/PageWrapper.tsx
+++ src/components/ui/PageWrapper.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from "react";
 
 const PageWrapper = ({ children }: PropsWithChildren) => {
-  return <div className="mx-auto max-w-4xl py-20">{children}</div>;
+  return <div className="mx-auto max-w-4xl py-12">{children}</div>;
 };
 
 export default PageWrapper;
