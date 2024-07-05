601ab0e7c6c809b8096fe5158d421fb59a6fb420
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-07-06 00:18:05 +0300

    feat: Add EmailTemplate component for generating email content

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -4,6 +4,7 @@ import { contactFormSchema } from "./schemas";
 import { initialFormState } from "./constants";
 import type { ContactForm } from "./types";
 import { Resend } from "resend";
+import { EmailTemplate } from "@/components/ui/EmailTemplate";
 
 export const sendEmail = async (
   formState: ContactForm,
@@ -31,11 +32,16 @@ export const sendEmail = async (
   try {
     const resend = new Resend(process.env.RESEND_API_KEY);
     await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
+      from: "Contact <onboarding@resend.dev>",
       to: process.env.MY_EMAIL!,
-      subject: "Contact form submission from " + data.name,
-      text: data.message,
+      subject: `Contact form submission from ${data.name}`,
+      react: EmailTemplate({
+        name: data.name,
+        email: data.email,
+        message: data.message,
+      }),
       reply_to: data.email,
+      text: `Contact form submission from ${data.name}. Please view this email in an HTML-compatible email client.`,
     });
     return {
       ...initialFormState,
