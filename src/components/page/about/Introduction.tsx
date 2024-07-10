34e6cbb9d42c29b8bfc2c11e4a273399df0dace0
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-07-10 22:01:05 +0300

    refactor: Add Introduction component to AboutPage

---
+++ src/components/page/about/Introduction.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Introduction = () => {
+  return (
+    <section className="mt-16">
+      <p className="text-slate-300">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et recusandae
+        perferendis accusantium deserunt natus soluta veniam mollitia obcaecati
+        animi ad ducimus, beatae similique aliquid distinctio non eos asperiores
+        placeat magni. Odio enim iste magnam aliquid alias exercitationem
+        molestias at, maiores numquam saepe dignissimos similique accusantium
+        fugit, dolores ratione modi aperiam nisi placeat reprehenderit officiis
+        minus! Quae iure excepturi quam! Possimus!
+      </p>
+      <br />
+      <p className="text-slate-300">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere
+        eligendi distinctio molestiae ab culpa natus libero, rem aliquam?
+        Obcaecati eaque sit ratione ab laudantium deleniti ipsum est. Est, quas
+        recusandae! Autem quis distinctio beatae temporibus, quibusdam
+        excepturi. A ut maiores minus quod omnis aperiam, aut esse maxime
+        quibusdam libero illum mollitia incidunt adipisci tenetur beatae
+        similique dolores. Dolores, eum obcaecati.
+      </p>
+      <br />
+      <p className="text-slate-300">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem natus
+        amet commodi architecto ratione doloribus quos, inventore sit dolorem
+        consequuntur nisi, velit odio libero. Obcaecati sit quam id adipisci
+        debitis. Aperiam fuga officiis recusandae consectetur facilis, et enim
+        autem vitae doloribus, mollitia itaque earum accusamus nostrum. Corrupti
+        placeat amet corporis vitae veniam tenetur laudantium at cumque animi?
+        Dolor, magnam ex.
+      </p>
+    </section>
+  );
+}
+
+export default Introduction
