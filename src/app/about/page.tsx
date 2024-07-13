a439553043670158d105b9df488384afacffd81c
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-07-13 10:50:37 +0300

    fix: Dynamic import WhyChoose to fix window issue

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,10 +2,17 @@ import Certifications from "@/components/page/about/Certifications";
 import Experience from "@/components/page/about/Experience";
 import Introduction from "@/components/page/about/Introduction";
 import Mastery from "@/components/page/about/Mastery";
-import WhyChoose from "@/components/page/about/WhyChoose";
 import PageHeader from "@/components/ui/PageHeader";
 import PageWrapper from "@/components/ui/PageWrapper";
+import dynamic from "next/dynamic";
 import React from "react";
+
+const WhyChoose = dynamic(
+  () => import("@/components/page/about/WhyChoose"),
+  {
+    ssr: false,
+  }
+);
 
 const AboutPage = () => {
   return (
