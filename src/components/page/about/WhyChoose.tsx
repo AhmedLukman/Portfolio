e04f11cd45a4b7f0745a3a9f89a4dce497375537
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-07-13 10:54:51 +0300

    refactor: Update WhyChoose section in AboutPage

--- src/components/page/about/WhyChoose.tsx
+++ src/components/page/about/WhyChoose.tsx
@@ -7,22 +7,19 @@ import { GLOBAL_WORLD_CONFIG, SAMPLE_ARCS } from '@/lib/data';
 const WhyChoose = () => {
   return (
     <section className="mt-14">
-      <SectionHeader className="text-center">Why i am fit</SectionHeader>
-      <div className="grid grid-cols-1 lg:grid-cols-3 mt-8 gap-4 max-w-7xl mx-auto w-full">
+      <SectionHeader className="text-center">Why I am fit</SectionHeader>
+      <div className="grid grid-cols-1 lg:grid-cols-3 mt-8 gap-4 w-full">
         <WobbleCard
           containerClassName="col-span-1 lg:col-span-2 h-full bg-pink-800 min-h-[500px] lg:min-h-[300px]"
-          className=""
         >
-          <div className="m ">
             <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
               Up-to-Date with Evolving Technology
             </h2>
-            <p className="mt-4 text-left  text-base/6 text-neutral-200">
+            <p className="mt-4 text-left text-base/6 text-neutral-200">
               I stay current with the latest trends and advancements in
               technology, continuously enhancing my skills to bring the best
               solutions to the table.
             </p>
-          </div>
         </WobbleCard>
         <WobbleCard containerClassName="col-span-1 min-h-[300px]">
           <h2 className="max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
