bbd7c69d06b56a7b748f923f57bcf984b167eed0
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-01-02 11:36:33 +0300

    feat: Add PageWrapper component for consistent layout structure

---
+++ src/app/components/ui/PageWrapper.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from "react";
+
+const PageWrapper = ({ children }: PropsWithChildren) => {
+  return (
+    <div className="mx-auto max-w-4xl py-10">{children}</div>
+  );
+};
+
+export default PageWrapper;
