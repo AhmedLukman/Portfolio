71023942eb0ba2f35606bc454fa5d3e9842937be
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-01-02 11:36:17 +0300

    feat: Create PageHeader wrapper component with GradientBorder

---
+++ src/app/components/ui/PageHeader.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@nextui-org/theme';
+import React, { PropsWithChildren } from 'react'
+import GradientBorder from './GradientBorder';
+
+const PageHeader = ({children, borderClassName = "max-w-xl"}: PropsWithChildren & {
+  borderClassName?: string
+}) => {
+  return (
+    <header>
+      <h1 className="text-xl md:text-2xl lg:text-3xl text-slate-200 text-center font-bold tracking-wide mb-1">
+        {children}
+      </h1>
+      <GradientBorder className={cn("mx-auto", borderClassName)} />
+    </header>
+  );
+}
+
+export default PageHeader
