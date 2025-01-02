f43770f89539a633f2c1949801f27f6f87069f20
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-01-02 14:01:11 +0300

    feat: Add Introduction component with developer profile and image

---
+++ src/app/components/page/Introduction.tsx
@@ -0,0 +1,33 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMedal } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
+
+const Introduction = () => {
+  return (
+    <section className="flex md:flex-row justify-between mt-14 gap-5 md:gap-0 flex-col-reverse">
+      <div className="md:basis-1/2 self-center">
+        <p className=" text-slate-300 text-center md:text-left">
+          I&apos;m a{" "}
+          <strong className="purple-gradient"> modern developer </strong>
+          with a distinguished Diploma in Business Information Technology{" "}
+          <FontAwesomeIcon icon={faMedal} /> Beyond academic excellence, I
+          possess a rich tapestry of certifications from top-tier courses and
+          boot-camps, alongside recognition in national media. My unwavering
+          dedication to mastering new skills and solving complex problems fuels
+          my passion for programming, making me an ideal candidate anywhere.
+        </p>
+      </div>
+      <div className="relative md:basis-1/2 h-52 md:h-64">
+        <Image
+          priority
+          sizes="(min-width: 768px) 50vw, 100vw"
+          fill
+          src="/assets/images/man-near-computer.svg"
+          alt="Animated picture of a white male sitting on a desk that has a computer"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Introduction;
