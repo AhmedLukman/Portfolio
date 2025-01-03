c5353354d1f33368f032f459a11a1d5c4f63c0b3
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-01-03 07:36:05 +0300

    style: Remove text alignment class for improved readability

--- src/app/components/page/Introduction.tsx
+++ src/app/components/page/Introduction.tsx
@@ -6,7 +6,7 @@ const Introduction = () => {
   return (
     <section className="flex md:flex-row justify-between mt-14 gap-5 md:gap-0 flex-col-reverse">
       <div className="md:basis-1/2 self-center">
-        <p className=" text-slate-300 text-center md:text-left">
+        <p className=" text-slate-300">
           I&apos;m a{" "}
           <strong className="purple-gradient"> modern developer </strong>
           with a distinguished Diploma in Business Information Technology{" "}
