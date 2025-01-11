959f661600400f52c77fd2c54ac732aa6c95268c
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-01-11 21:55:32 +0300

    fix: Remove unnecessary class

--- src/app/components/page/home-page/AnimatedTestimonials.tsx
+++ src/app/components/page/home-page/AnimatedTestimonials.tsx
@@ -48,7 +48,7 @@ export const AnimatedTestimonials = ({
   };
 
   return (
-    <div className="max-w-sm md:max-w-4xl mx-auto antialiased font-sans px-4 md:px-8 lg:px-12">
+    <div className="max-w-sm md:max-w-4xl mx-auto antialiased px-4 md:px-8 lg:px-12">
       <div className="relative grid grid-cols-1 md:grid-cols-2  gap-20">
         <div>
           <div className="relative h-80 w-full">
