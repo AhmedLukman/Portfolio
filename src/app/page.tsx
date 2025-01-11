c07f77ff5c9e85f4cd9d0690b0e400540905d245
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-01-11 21:58:11 +0300

    feat: Implement home page with created components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import Introduction from "./components/page/home-page/Introduction";
+import AnimatedWords from "./components/page/home-page/AnimatedWords";
+import PageWrapper from "./components/wrappers/PageWrapper";
+import TechAndTools from "./components/page/home-page/TechAndTools";
+import Testimonials from "./components/page/home-page/Testimonials";
+import CallToAction from "./components/page/home-page/CallToAction";
+import MainHeading from "./components/page/home-page/MainHeading";
+
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <PageWrapper>
+      <MainHeading />
+      <Introduction />
+      <TechAndTools />
+      <Testimonials />
+      <AnimatedWords />
+      <CallToAction />
+    </PageWrapper>
+  );
+};
 
-export default Home
+export default Home;
