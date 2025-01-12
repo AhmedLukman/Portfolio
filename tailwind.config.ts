d7415e1453e025b499bad5eae78f47af552547ec
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-01-12 21:35:17 +0300

    chore: Add @nextui-org/avatar package and update Tailwind config for new component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,7 +22,7 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(button|link|ripple|spinner).js"
+    "./node_modules/@nextui-org/theme/dist/components/(avatar|button|link|ripple|spinner).js"
   ],
   theme: {
     extend: {
