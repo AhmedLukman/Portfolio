0b53b7b50d72f42d4ceae9834c9bdd3f721f2b08
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-01-12 22:07:04 +0300

    feat: Add SideNavFooter component

---
+++ src/app/components/ui/side-nav/SideNavFooter.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@nextui-org/button";
+
+const SideNavFooter = () => {
+  return (
+    <footer className="flex justify-center">
+      <Button
+        variant="shadow"
+        className="text-white bg-gradient-to-br from-indigo-500 to-pink-600 border-small border-white/50 shadow-pink-500/30"
+      >
+        Download CV
+      </Button>
+    </footer>
+  );
+};
+
+export default SideNavFooter;
