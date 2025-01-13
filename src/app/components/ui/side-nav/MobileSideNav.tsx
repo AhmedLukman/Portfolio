99556da107f3bb266b9b2116c337f10d4c36efba
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-01-13 15:04:47 +0300

    feat: Add MobileSideNav component for mobile navigation

---
+++ src/app/components/ui/side-nav/MobileSideNav.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { Button } from "@nextui-org/button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import SideNavContent from "./SideNavContent";
+import { useState } from "react";
+
+const MobileSideNav = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  return (
+    <>
+      <div className="lg:hidden fixed top-4 right-4 z-50">
+        <Button
+          isIconOnly
+          variant="light"
+          onPress={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+          className="bg-[#262c35]/80 text-white"
+        >
+          <FontAwesomeIcon
+            icon={isMobileMenuOpen ? faXmark : faBars}
+            className="h-5 w-5"
+          />
+        </Button>
+      </div>
+      <AnimatePresence>
+        {isMobileMenuOpen && (
+          <motion.aside
+            initial={{ x: "-100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "-100%" }}
+            transition={{ type: "tween", duration: 0.3 }}
+            className="fixed inset-0 w-full h-full bg-[#0a0e14]/50 backdrop-blur-xl z-40 lg:hidden flex flex-col justify-between p-6 overflow-y-auto"
+          >
+            <SideNavContent />
+          </motion.aside>
+        )}
+      </AnimatePresence>
+    </>
+  );
+};
+
+export default MobileSideNav;
