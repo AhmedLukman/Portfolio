cfd5270107d4df32ddf8b83e6682a10055fdf63e
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-01-13 15:05:01 +0300

    feat: Add SideNavContent component to structure side navigation layout

---
+++ src/app/components/ui/side-nav/SideNavContent.tsx
@@ -0,0 +1,15 @@
+import SideNavLinks from "./SideNavLinks";
+import SideNavHeader from "./SideNavHeader";
+import SideNavFooter from "./SideNavFooter";
+
+const SideNavContent = () => {
+  return (
+    <>
+      <SideNavHeader />
+      <SideNavLinks />
+      <SideNavFooter />
+    </>
+  );
+};
+
+export default SideNavContent;
