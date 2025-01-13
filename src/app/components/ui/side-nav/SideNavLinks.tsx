ff1cbca842035c1ab54298d6eb2eee5e3eb51ec5
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-01-13 15:18:38 +0300

    feat: Add SideNavLinks component to display navigation and social links

---
+++ src/app/components/ui/side-nav/SideNavLinks.tsx
@@ -0,0 +1,32 @@
+import { PAGE_LINKS, SOCIAL_LINKS } from "@/app/lib/constants";
+import SideNavLinkButton from "./SideNavLinkButton";
+
+const SideNavLinks = () => {
+  return (
+    <nav className="flex flex-col justify-evenly h-full">
+      <div className="flex flex-col gap-3">
+        <h3 className="text-white">Pages</h3>
+        <ul className="flex flex-col gap-3">
+          {PAGE_LINKS.map((link) => (
+            <li key={link.path}>
+              <SideNavLinkButton link={link} />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <h3 className="text-white">Socials</h3>
+        <ul className="flex flex-col gap-3">
+          {SOCIAL_LINKS.map((link) => (
+            <li key={link.path}>
+              <SideNavLinkButton link={link} isSocial />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default SideNavLinks;
