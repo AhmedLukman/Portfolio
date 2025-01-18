fb32aa82a7753185c7d31ad9f98cd79cf324b970
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-01-19 00:03:48 +0300

    refactor: Place button classes outside element

--- src/app/components/ui/side-nav/SideNavLinkButton.tsx
+++ src/app/components/ui/side-nav/SideNavLinkButton.tsx
@@ -18,24 +18,32 @@ const SideNavLinkButton = ({
   const pathname = usePathname();
   const isActive =
     link.path === "/" ? pathname === link.path : pathname.startsWith(link.path);
+
+  const baseButtonClasses =
+    "!text-slate-400 group !bg-transparent w-full justify-start";
+  const transitionClasses = "!transition-all !duration-200";
+  const hoverClasses =
+    "lg:hover:!text-white lg:hover:!bg-gradient-to-b lg:hover:!from-[#653e80] lg:hover:!to-[#7a5299] !ml-0 lg:hover:!ml-5";
+  const activeClasses =
+    "lg:hover:!opacity-100 lg:!ml-5 shadow-[0_8px_16px_rgb(0_0_0/0.3)] bg-gradient-to-b from-[#653e80] to-[#7a5299] !text-white";
+
+  const iconClasses = cn(
+    "!text-slate-400 group-hover:!text-white !transition-all !duration-200 h-4 mr-1",
+    { "!text-white": isActive }
+  );
+
+  const externalIconClasses =
+    "ml-auto !opacity-0 group-hover:!opacity-100 !transition !duration-200";
+
   return (
     <Button
-      key={link.path}
       startContent={
-        <FontAwesomeIcon
-          className={cn(
-            "!text-slate-400 group-hover:!text-white !transition-all !duration-200 h-4 mr-1",
-            {
-              "!text-white": isActive,
-            }
-          )}
-          icon={link.icon}
-        />
+        <FontAwesomeIcon className={iconClasses} icon={link.icon} />
       }
       endContent={
         isSocial && (
           <FontAwesomeIcon
-            className="ml-auto !opacity-0 group-hover:!opacity-100 !transition !duration-200"
+            className={externalIconClasses}
             icon={faArrowUpRightFromSquare}
           />
         )
@@ -43,13 +51,9 @@ const SideNavLinkButton = ({
       as={Link}
       href={link.path}
       variant={isActive ? "solid" : "light"}
-      className={cn(
-        "!text-slate-400 group !bg-transparent lg:hover:!text-white lg:hover:!bg-gradient-to-b lg:hover:!from-[#653e80] lg:hover:!to-[#7a5299] !ml-0 lg:hover:!ml-5 !transition-all !duration-200 w-full justify-start",
-        {
-          "lg:hover:!opacity-100 lg:!ml-5 shadow-[0_8px_16px_rgb(0_0_0/0.3)] bg-gradient-to-b from-[#653e80] to-[#7a5299] !text-white":
-            isActive,
-        }
-      )}
+      className={cn(baseButtonClasses, transitionClasses, hoverClasses, {
+        [activeClasses]: isActive,
+      })}
       isExternal={isSocial}
     >
       {link.name}
