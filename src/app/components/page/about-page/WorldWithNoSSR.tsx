80e03109efae973d73debb16b6aca4a497338154
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-02-16 16:31:14 +0300

    refactor: Remove WorldWithNoSSR and Globe components to streamline About page

--- src/app/components/page/about-page/WorldWithNoSSR.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-export const WorldWithNoSSR = dynamic(
-  () =>
-    import("@/app/components/page/about-page/Globe").then((m) => m.World),
-  { ssr: false }
-);
