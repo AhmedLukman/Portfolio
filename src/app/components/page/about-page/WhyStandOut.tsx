28c8c7f858bd103344b517ad24d76677fcb86c53
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-02-23 16:58:20 +0300

    chore: Change heading tags from h2 to h3 in WhyStandOut component for semantic consistency

--- src/app/components/page/about-page/WhyStandOut.tsx
+++ src/app/components/page/about-page/WhyStandOut.tsx
@@ -8,9 +8,9 @@ export default function WhyStandOut() {
       <SectionHeader className="text-center">Why I stand out</SectionHeader>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-8 gap-4">
         <WobbleCard containerClassName="col-span-1 lg:col-span-2 h-full bg-pink-800">
-          <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-slate-100">
+          <h3 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-slate-100">
             Up-to-Date with Evolving Technology
-          </h2>
+          </h3>
           <p className="mt-4 text-left text-base/6 text-slate-200">
             I stay current with the latest trends and advancements in
             technology, continuously enhancing my skills to bring the best
@@ -18,9 +18,9 @@ export default function WhyStandOut() {
           </p>
         </WobbleCard>
         <WobbleCard containerClassName="col-span-1">
-          <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-slate-100">
+          <h3 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-slate-100">
             User centric
-          </h2>
+          </h3>
           <p className="mt-4 text-left text-base/6 text-slate-200">
             I create intuitive, user-focused solutions by blending technical
             skill with a deep understanding of user behavior.
@@ -28,9 +28,9 @@ export default function WhyStandOut() {
         </WobbleCard>
         <WobbleCard containerClassName="col-span-1 sm:col-span-2 lg:col-span-3 bg-blue-900">
             <div className="sm:w-1/2">
-              <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-slate-100">
+              <h3 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-slate-100">
                 Flexible with different Time Zone
-              </h2>
+              </h3>
               <p className="mt-6 mb-20 text-left text-base/6 text-slate-200">
                 I am adept at accommodating various time zones, ensuring
                 seamless collaboration with global teams and clients regardless
