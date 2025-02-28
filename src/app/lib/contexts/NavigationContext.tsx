e383a25493c11a68202fde670a0a7f0d5f322c70
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-03-01 00:20:17 +0300

    feat: Implement NavigationContext for handling menu state

---
+++ src/app/lib/contexts/NavigationContext.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { createContext, useContext, useState, PropsWithChildren } from "react";
+
+type NavigationContextType = {
+  isMobileMenuOpen: boolean;
+  setIsMobileMenuOpen: (isOpen: boolean) => void;
+  closeMobileMenu: () => void;
+};
+
+const NavigationContext = createContext<NavigationContextType | undefined>(
+  undefined
+);
+
+export const NavigationProvider = ({ children }: PropsWithChildren) => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const closeMobileMenu = () => setIsMobileMenuOpen(false);
+
+  return (
+    <NavigationContext.Provider
+      value={{ isMobileMenuOpen, setIsMobileMenuOpen, closeMobileMenu }}
+    >
+      {children}
+    </NavigationContext.Provider>
+  );
+};
+
+export const useNavigation = () => {
+  const context = useContext(NavigationContext);
+  if (context === undefined)
+    throw new Error("useNavigation must be used within a NavigationProvider");
+  return context;
+};
