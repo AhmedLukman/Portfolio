b10dff0818564b7a1f91827d7ffcf460a94dc7e8
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-03-01 20:56:38 +0300

    feat: Add sitemap file

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://ahmedlukman.dev',
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://ahmedlukman.dev/about',
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://ahmedlukman.dev/projects',
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://ahmedlukman.dev/contact',
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+  ]
+}
