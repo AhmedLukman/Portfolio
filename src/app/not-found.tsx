081f392ecab4d6944609dba5919d139ed1675fb6
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-03-16 17:56:01 +0300

    feat: Add new image template with text

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,25 @@
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 
 const NotFound = () => {
   return (
-    <div className="h-full flex justify-center items-center">
-      <Image
-        src="/assets/images/404-image.png"
-        width={500}
-        height={500}
-        priority
-        alt="404 page not found image illustration"
-      />
+    <div className=" h-full flex justify-center items-center flex-col">
+      <div className="relative">
+        <Image
+          src="/assets/images/portfolio-image-template.png"
+          width={500}
+          height={500}
+          priority
+          alt="Error image illustration"
+        />
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-3xl text-center flex flex-col gap-1 items-center">
+          <p className="text-heading">
+            404 <FontAwesomeIcon icon={faTriangleExclamation} />
+          </p>
+          <p>Page not found</p>
+        </div>
+      </div>
     </div>
   );
 };
