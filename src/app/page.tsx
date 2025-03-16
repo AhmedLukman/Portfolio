29e11082d87b1e29075bf4e0492761ca32fd4a16
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-03-16 06:27:12 +0300

    refactor: Swap testimonials and animated words

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ const Home = () => {
       <MainHeading />
       <Introduction />
       <TechAndTools />
-      <Testimonials />
       <AnimatedWords />
+      <Testimonials />
       <CallToAction />
     </PageWrapper>
   );
