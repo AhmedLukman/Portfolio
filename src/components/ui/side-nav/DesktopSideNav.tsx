af2b3e52d9da78202ed2a47700bec2af8bc66778
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-05-25 22:02:06 +0300

    style: Update side nav width for better responsiveness

--- src/components/ui/side-nav/DesktopSideNav.tsx
+++ src/components/ui/side-nav/DesktopSideNav.tsx
@@ -2,7 +2,7 @@ import SideNavContent from "./SideNavContent";
 
 const DesktopSideNav = () => {
   return (
-    <aside className="basis-2/12 hidden lg:block px-10 py-4 xl:p-10 shadow shadow-gray-800 rounded-md z-10">
+    <aside className="basis-3/12 hidden lg:block 2xl:basis-2/12 px-10 py-4 xl:p-10 shadow shadow-gray-800 rounded-md z-10">
       <SideNavContent />
     </aside>
   );
