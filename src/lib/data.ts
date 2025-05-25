8db21a78f80a1eac48236d87e8d49cf60770bc19
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-05-25 23:03:52 +0300

    fix: Update start dates for experience entries

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -595,7 +595,7 @@ export const EXPERIENCE: {
     description:
       "Collaborated adeptly with UI/UX designers and fellow engineers to craft clean, high-performance front-end web solutions. Key contributions to projects like Adapta, SSAMS, Digital Learning, and Next Events. Recognized as the Lead Intern, entrusted with independent design responsibilities due to a trusted sense of aesthetic.",
     company: "@iLabAfrica",
-    startDate: "2023-03",
+    startDate: "2023-02",
     endDate: "2023-08",
     companyLogo: IlabAfrica,
   },
@@ -605,7 +605,7 @@ export const EXPERIENCE: {
     description:
       "Developed aesthetically pleasing UI components for the web, integrating dynamic features such as drag and drop, and seamlessly incorporated APIs using Next.js and Express. Recognized for achieving high performance and receiving praise for design excellence.",
     company: "Trinetium DMCC",
-    startDate: "2022-8",
+    startDate: "2022-07",
     endDate: "2022-10",
     companyLogo: Trinetium,
   },
