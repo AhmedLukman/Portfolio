030678bcf180b363a47ec8476a100583fde8e5d8
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-05-29 14:11:25 +0300

    feat: Implement starting project page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,22 @@
+
+import ProjectsList from "@/components/page/projects-page/ProjectsList";
+import PageHeader from "@/components/wrappers/PageHeader";
+import PageWrapper from "@/components/wrappers/PageWrapper";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Ahmed Lukman | Projects",
+  description:
+    "A showcase of projects and work completed by Ahmed Lukman, highlighting skills and achievements.",
+};
+
+const ProjectsPage = () => {
+  return (
+    <PageWrapper>
+      <PageHeader>My Projects</PageHeader>
+      <ProjectsList />
+    </PageWrapper>
+  );
+};
+
+export default ProjectsPage;
