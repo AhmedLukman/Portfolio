49149e2d02e81b9e9f018128a85cb92cc24b0e61
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-06-07 22:13:15 +0300

    feat: Add responsive image sizes

--- src/components/page/projects-page/ProjectOverview.tsx
+++ src/components/page/projects-page/ProjectOverview.tsx
@@ -36,6 +36,7 @@ const ProjectOverview = ({
         <Image
           src={image}
           alt={title}
+          sizes="(min-width: 1540px) 492px, (min-width: 1280px) 438px, (min-width: 1040px) calc(26.36vw + 66px), (min-width: 980px) 816px, (min-width: 780px) calc(72.78vw + 117px), calc(100vw - 55px)"
           fill
           priority
           className="object-contain object-top animate-slideInFromLeft"
@@ -43,21 +44,15 @@ const ProjectOverview = ({
       </div>
       <div className="flex flex-col basis-1/2 justify-between gap-10">
         <div className="flex flex-col gap-1 animate-slideInFromBottom">
-          <h2 className="text-xl purple-gradient font-semibold">
-            Description
-          </h2>
+          <h2 className="text-xl purple-gradient font-semibold">Description</h2>
           <p className="text-slate-300">{description}</p>
         </div>
         <div className="flex flex-col gap-3 opacity-0 animate-slideInFromBottom delay-500">
-          <h2 className="text-xl purple-gradient font-semibold">
-            Tech Stack
-          </h2>
+          <h2 className="text-xl purple-gradient font-semibold">Tech Stack</h2>
           <AnimatedTooltip items={techStack} />
         </div>
         <div className="flex flex-col gap-3 opacity-0 animate-slideInFromBottom delay-1000">
-          <h2 className="text-xl purple-gradient font-semibold">
-            Metrics
-          </h2>
+          <h2 className="text-xl purple-gradient font-semibold">Metrics</h2>
           <AnimatedTooltip items={metrics} />
         </div>
         <div className="flex justify-between opacity-0 animate-slideInFromBottom delay-1500">
@@ -79,9 +74,7 @@ const ProjectOverview = ({
             </div>
           </div>
           <div className="flex flex-col gap-1">
-            <h2 className="text-xl purple-gradient font-semibold">
-              End Date
-            </h2>
+            <h2 className="text-xl purple-gradient font-semibold">End Date</h2>
             <div className="flex items-center gap-2">
               <FontAwesomeIcon
                 icon={faCalendarAlt}
@@ -96,9 +89,7 @@ const ProjectOverview = ({
             </div>
           </div>
           <div className="flex flex-col gap-1">
-            <h2 className="text-xl purple-gradient font-semibold">
-              Status
-            </h2>
+            <h2 className="text-xl purple-gradient font-semibold">Status</h2>
             <StatusChip status={status} />
           </div>
         </div>
