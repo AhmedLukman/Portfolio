3f3fd9faa5dbd4dab4ea154b8778bc326fa2aa3e
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-06-11 06:38:51 +0300

    fix: Add missing operatingSystem field to ProjectListSchema

--- src/lib/schemas/ProjectListSchema.tsx
+++ src/lib/schemas/ProjectListSchema.tsx
@@ -24,6 +24,7 @@ export default function ProjectListSchema() {
           name: "Ahmed Lukman",
         },
         applicationCategory: "WebApplication",
+        operatingSystem: "Any",
       },
     })),
   };
