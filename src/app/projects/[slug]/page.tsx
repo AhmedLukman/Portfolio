7eb17ec03c79dcd13da29f87130488b2ba1461c8
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-06-11 06:23:15 +0300

    feat: Add ProjectSchema component for structured data in project details

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -4,6 +4,7 @@ import ProjectOverview from "@/components/page/projects-page/ProjectOverview";
 import PageHeader from "@/components/wrappers/PageHeader";
 import PageWrapper from "@/components/wrappers/PageWrapper";
 import { PROJECTS } from "@/lib/data";
+import ProjectSchema from "@/lib/schemas/ProjectSchema";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
@@ -33,6 +34,14 @@ const ProjectDetailsPage = async ({
   if (!project) notFound();
   return (
     <PageWrapper className="2xl:max-w-5xl">
+      <ProjectSchema
+        name={project.overview.title}
+        description={project.overview.description}
+        url={`https://ahmedlukman.dev/projects/${project.slug}`}
+        image={project.overview.image?.src}
+        datePublished={project.overview.startDate?.toISOString?.()}
+        codeRepository={project.overview.source}
+      />
       <PageHeader>{project.overview.title}</PageHeader>
       <ProjectOverview project={project} />
       <CaseStudy project={project} />
---
+++ src/lib/schemas/ProjectSchema.tsx
@@ -0,0 +1,42 @@
+import Script from "next/script";
+
+type ProjectSchemaProps = {
+  name: string;
+  description: string;
+  url: string;
+  image?: string;
+  datePublished?: string;
+  codeRepository: string;
+}
+
+export default function ProjectSchema({
+  name,
+  description,
+  url,
+  image,
+  datePublished,
+  codeRepository,
+}: ProjectSchemaProps) {
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "SoftwareApplication",
+    name: name,
+    description: description,
+    url: url,
+    ...(image && { image }),
+    ...(datePublished && { datePublished }),
+    author: {
+      "@type": "Person",
+      name: "Ahmed Lukman",
+    },
+    codeRepository,
+    applicationCategory: "WebApplication",
+    operatingSystem: "All",
+  };
+
+  return (
+    <Script id="schema-project" type="application/ld+json">
+      {JSON.stringify(schema)}
+    </Script>
+  );
+}
