698653b75021c30e95d0ab1612457b01d3660ae8
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-07-08 09:27:07 +0300

    feat: Add additional routes and simplify properties

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,43 +1,36 @@
 import { PORTFOLIO_SITE } from "@/lib/constants"
+import { PROJECTS } from "@/lib/data"
 import { MetadataRoute } from "next"
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
+  const baseRoutes: MetadataRoute.Sitemap = [
     {
       url: PORTFOLIO_SITE,
-      lastModified: new Date().toISOString(),
       changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: `${PORTFOLIO_SITE}/about`,
-      lastModified: new Date().toISOString(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: `${PORTFOLIO_SITE}/projects`,
-      lastModified: new Date().toISOString(),
       changeFrequency: "monthly",
       priority: 0.9,
     },
     {
       url: `${PORTFOLIO_SITE}/contact`,
-      lastModified: new Date().toISOString(),
       changeFrequency: "monthly",
       priority: 0.7,
     },
-    {
-      url: `${PORTFOLIO_SITE}/projects/portfolio`,
-      lastModified: new Date().toISOString(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: `${PORTFOLIO_SITE}/projects/cinemania`,
-      lastModified: new Date().toISOString(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
   ]
+
+  const projectRoutes: MetadataRoute.Sitemap = PROJECTS.map((project) => ({
+    url: `${PORTFOLIO_SITE}/projects/${project.slug}`,
+    changeFrequency: "monthly",
+    priority: 0.8,
+  }))
+
+  return [...baseRoutes, ...projectRoutes]
 }
