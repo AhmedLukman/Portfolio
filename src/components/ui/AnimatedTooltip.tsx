73cfbcc1b35f8b96069ddee83a51c32311b28463
Ahmed Lukman <itsahmedlukman@gmail.com>
2025-07-19 12:37:05 +0300

    fix: Adjust tooltip positioning

--- src/components/ui/AnimatedTooltip.tsx
+++ src/components/ui/AnimatedTooltip.tsx
@@ -77,7 +77,7 @@ export const AnimatedTooltip = ({
                   rotate: rotate,
                   whiteSpace: "nowrap",
                 }}
-                className="absolute -left-1/2 -top-16 z-50 flex translate-x-1/2 flex-col items-center justify-center rounded-md bg-black px-4 py-2 text-xs shadow-xl"
+                className="absolute -top-16 left-1/2 z-50 flex -translate-x-1/2 flex-col items-center justify-center rounded-md bg-black px-4 py-2 text-xs shadow-xl"
               >
                 <div className="absolute inset-x-10 -bottom-px z-30 h-px w-[20%] bg-linear-to-r from-transparent via-emerald-500 to-transparent" />
                 <div className="absolute -bottom-px left-10 z-30 h-px w-[40%] bg-linear-to-r from-transparent via-sky-500 to-transparent" />
@@ -103,7 +103,7 @@ export const AnimatedTooltip = ({
               >
                 <FontAwesomeIcon
                   icon={rest.icon}
-                  className="h-full! w-full! text-body"
+                  className="text-body h-full! w-full!"
                 />
               </span>
             )}
